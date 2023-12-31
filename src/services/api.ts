9cad70f520432bbc1fa0edcdbedd7df343b8eb31
Gabriel Cabeceira <gabrielcabeceira@gmail.com>
2023-12-31 15:43:42 -0300

    added a domain to API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = 'https://54.221.54.121:443';
+export const url = 'https://ec2-54-221-54-121.compute-1.amazonaws.com:443';
 
 export const api = axios.create({
   baseURL: url
